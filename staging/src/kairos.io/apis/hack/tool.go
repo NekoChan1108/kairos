6321b37c38d1f9eed48e9e176ee018a1139d34ef
WuZiyang <wuziyang2020@126.com>
2026-09-26 19:28:41 +0800

    chore(kairos): add codegen for apis

--- staging/src/kairos.io/apis/hack/tool.go
+++ staging/src/kairos.io/apis/hack/tool.go
@@ -1,7 +1,7 @@
 //go:build tools
 
 /*
-Copyright 2026 The kairos Authors.
+Copyright 2026 The Kairos Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
